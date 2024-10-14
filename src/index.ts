369dddbdedb8bda59fa324f58c5be28b1f9df9ff
Sofen Hoque <sofen.hoque@agoda.com>
2024-10-14 14:45:13 +0700

    add vite plugin code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import { NormalizedOutputOptions, OutputBundle } from 'rollup';
+
+export function enforceBootstrapBundleSize(
+  maxSizeKb: number,
+  additionalInstructionsMsg?: string
+) {
+  return {
+    name: 'enforce-bootstrap-bundle-size-limit',
+
+    generateBundle: (
+      outputOptions: NormalizedOutputOptions,
+      outputBundle: OutputBundle
+    ) => {
+      for (const [_, bundle] of Object.entries(outputBundle)) {
+        if (bundle.name === 'bootstrap' && bundle.type === 'chunk') {
+          const sizeBytes = new Blob([bundle.code]).size;
+
+          if (sizeBytes > maxSizeKb * 1000) {
+            // eslint-disable-next-line max-len
+            const errorMsg = `The bootstrap bundle size (${
+              sizeBytes / 1000
+            }kB) exceeds limit ${maxSizeKb}kB. ${additionalInstructionsMsg ?? ''}`;
+            throw new Error(errorMsg);
+          }
+        }
+      }
+    },
+  };
+}
